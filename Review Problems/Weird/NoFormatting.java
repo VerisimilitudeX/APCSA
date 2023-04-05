457ce70128f0102d3d5df1e5af9387d1e6a24fb3
Piyush Acharya <acharyapiyush1@outlook.com>
2023-04-04 18:18:16 -0700

    Create NoFormatting.java

---
+++ Review Problems/Weird/NoFormatting.java
@@ -0,0 +1,11 @@
+package Weird;
+
+public class NoFormatting {
+    public static void main(String[] args) {
+        int num = 22;
+        if (num > 0)
+        if (num % 5 == 0)
+        System.out.println(num);
+        else System.out.println(num + " is negative");
+    }
+}
